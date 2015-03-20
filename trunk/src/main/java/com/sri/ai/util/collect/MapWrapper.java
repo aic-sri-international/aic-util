edfb66a3157213a99fdacbf4ba08355ee602953e
rodrigobraz <rodrigobraz@68f7db96-9938-682f-d433-21a6cbf682cc>
2015-03-20 02:02:01 +0000

    - removed NonEqualitiesConstraint's extension of Map.

---
+++ trunk/src/main/java/com/sri/ai/util/collect/MapWrapper.java
@@ -0,0 +1,77 @@
+package com.sri.ai.util.collect;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * A Map that wraps a given map. Useful as a basis for classes that need to modify certain behaviors of a given map.
+ */
+public class MapWrapper<K, V> implements Map<K,V> {
+
+	private Map<K,V> map;
+	
+	public MapWrapper(Map<K,V> map) {
+		this.map = map;
+	}
+	
+	@Override
+	public void clear() {
+		map.clear();
+	}
+
+	@Override
+	public boolean containsKey(Object arg0) {
+		return map.containsKey(arg0);
+	}
+
+	@Override
+	public boolean containsValue(Object arg0) {
+		return map.containsValue(arg0);
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<K, V>> entrySet() {
+		return map.entrySet();
+	}
+
+	@Override
+	public V get(Object arg0) {
+		return map.get(arg0);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return map.isEmpty();
+	}
+
+	@Override
+	public Set<K> keySet() {
+		return map.keySet();
+	}
+
+	@Override
+	public V remove(Object arg0) {
+		return map.remove(arg0);
+	}
+
+	@Override
+	public int size() {
+		return map.size();
+	}
+
+	@Override
+	public V put(K key, V value) {
+		return map.put(key, value);
+	}
+
+	@Override
+	public void putAll(Map<? extends K, ? extends V> m) {
+		map.putAll(m);
+	}
+
+	@Override
+	public Collection<V> values() {
+		return map.values();
+	}
+}
