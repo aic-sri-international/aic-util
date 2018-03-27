bd7e01c4605c4e2000f528b54a1a7d5481dae699
Gabriel Azevedo Ferreira <gabriel.azevedo-ferreira@polytechnique.edu>
2018-03-27 12:08:17 +0800

    start of plotting library

--- src/main/java/com/sri/ai/util/rplot/Ggplot.java
+++ src/main/java/com/sri/ai/util/rplot/Ggplot.java
@@ -3,8 +3,11 @@ package com.sri.ai.util.rplot;
 import static com.sri.ai.util.Util.println;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.rosuda.REngine.REngineException;
+import org.rosuda.REngine.RList;
 //import org.rosuda.REngine.Rserve.*;
 import org.rosuda.REngine.Rserve.RConnection;
 import org.rosuda.REngine.Rserve.RserveException;
@@ -21,9 +24,8 @@ import org.rosuda.REngine.Rserve.RserveException;
  * @author gabriel
  *
  */
-public class ggplot {
+public class Ggplot {
 	
-	//
 	public static void ggplotInit()  {
     	RConnection connection = null;
     
@@ -43,8 +45,53 @@ public class ggplot {
         }
 	}
 
-	public static void ggplotPlot(String cmd, List<String> args) {
-		//TODO
+	public static void ggplotPlot(String ggplotCmd, 
+			List<String> dfNames,List<double[]> dfColumns,List<String> ggplotArgs) {
+		RConnection c = null;
+		try {
+			c = new RConnection();
+			
+			ArrayList<String> ListOfColNames = createColumnsInR(dfColumns, c);
+			
+			String listsIndexesConcatenated = parseToOneString(ListOfColNames);
+			String args = parseToOneString(ggplotArgs);
+			
+			c.eval("df<-data.frame(" + listsIndexesConcatenated + ")");
+			c.eval("p <-" + ggplotCmd + "(df," + args + ")");
+		} catch (RserveException e) {
+			e.printStackTrace();
+		} catch (REngineException e) {
+			e.printStackTrace();
+		}finally {
+			c.close();
+		}
+	}
+
+	private static String parseToOneString(List<String> listOfColNames) {
+		String res = "";
+		
+		boolean first = true;
+		for(String s : listOfColNames) {
+			if(first) {
+				res = s;
+				first = false;
+			}
+			else{
+				res = res + "," + s;
+			}
+		}
+		return res;
+	}
+
+	private static ArrayList<String> createColumnsInR(List<double[]> dfColumns, RConnection c) throws REngineException {
+		int i = 0;
+		ArrayList<String> s = new ArrayList<>();
+		for(double[] col : dfColumns) {
+			c.assign("l"+i, col);
+			s.add(s + "l");
+			i++;
+		}
+		return s;
 	}
 	
 	//Test code
