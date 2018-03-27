c775373b09287a2e9faf478df8d65baa013c5322
Rodrigo de Salvo Braz <braz@ai.sri.com>
2018-03-26 22:19:32 -0700

    - small fixes

--- src/main/java/com/sri/ai/util/rplot/StartRserve.java
+++ src/main/java/com/sri/ai/util/rplot/StartRserve.java
@@ -78,6 +78,7 @@ public class StartRserve {
   /**
    * shortcut to
    * <code>launchRserve(cmd, "--no-save --slave", "--no-save --slave", false)</code>
+   * @return the value returned by <code>launchRserve(cmd, "--no-save --slave", "--no-save --slave", false)</code>
    */
   public static boolean launchRserve(String cmd) {
     return launchRserve(cmd, "--no-save --slave", "--no-save --slave", false);
@@ -90,6 +91,7 @@ public class StartRserve {
    * @param cmd command necessary to start R
    * @param rargs arguments are are to be passed to R
    * @param rsrvargs arguments to be passed to Rserve
+   * @param debug whether to debug
    * @return <code>true</code> if Rserve is running or was successfully started,
    * <code>false</code> otherwise.
    */
@@ -110,7 +112,9 @@ public class StartRserve {
       }
       System.out.println("StartRserve: waiting for Rserve to start ... (" + p + ")");
       // we need to fetch the output - some platforms will die if you don't ...
+      @SuppressWarnings("unused")
       StreamHog errorHog = new StreamHog(p.getErrorStream(), false);
+      @SuppressWarnings("unused")
       StreamHog outputHog = new StreamHog(p.getInputStream(), false);
       if (!isWindows) /* on Windows the process will never return, so we cannot wait */ {
         p.waitFor();
@@ -145,7 +149,8 @@ public class StartRserve {
    * start it using the defaults for the platform where it is run on. This
    * method is meant to be set-and-forget and cover most default setups. For
    * special setups you may get more control over R with
-   * <<code>launchRserve</code> instead.
+   * <code>launchRserve</code> instead.
+   * @return whether it succeed.
    */
   public static boolean checkLocalRserve() {
     if (isRserveRunning()) {
@@ -201,6 +206,7 @@ public class StartRserve {
 
   /**
    * just a demo main method which starts Rserve and shuts it down again
+   * @param args the arguments
    */
   public static void main(String[] args) {
     System.out.println("result=" + checkLocalRserve());
