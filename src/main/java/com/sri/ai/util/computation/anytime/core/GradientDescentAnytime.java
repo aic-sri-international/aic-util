2d1791d677ea5be0541a1f3fc05ca026faec9951
dziri <dziri@Fresno.AI.SRI.COM>
2018-04-26 10:02:35 -0700

    Implemented first layer of Approximation with gradient descent in aic-util

---
+++ src/main/java/com/sri/ai/util/computation/anytime/core/GradientDescentAnytime.java
@@ -0,0 +1,5 @@
+package com.sri.ai.util.computation.anytime.core;
+
+public class GradientDescentAnytime<T> {
+
+}
---
+++ src/main/java/com/sri/ai/util/computation/anytime/core/GradientDescentApproximation.java
@@ -0,0 +1,7 @@
+package com.sri.ai.util.computation.anytime.core;
+
+import com.sri.ai.util.computation.anytime.api.Approximation;
+
+public class GradientDescentApproximation<T> implements Approximation<T> {
+
+}
