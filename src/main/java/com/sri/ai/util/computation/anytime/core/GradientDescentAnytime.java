7f5b4c5506256e4bbef6d7964696e1cd54eeade2
dziri <dziri@Fresno.AI.SRI.COM>
2018-04-26 17:36:09 -0700

    Few changes to gradient descent computation, creation of appropriate packages

--- src/main/java/com/sri/ai/util/computation/anytime/core/GradientDescentAnytime.java
+++
@@ -1,5 +0,0 @@
-package com.sri.ai.util.computation.anytime.core;
-
-public class GradientDescentAnytime<T> {
-
-}
--- src/main/java/com/sri/ai/util/computation/anytime/core/GradientDescentApproximation.java
+++
@@ -1,7 +0,0 @@
-package com.sri.ai.util.computation.anytime.core;
-
-import com.sri.ai.util.computation.anytime.api.Approximation;
-
-public class GradientDescentApproximation<T> implements Approximation<T> {
-
-}
