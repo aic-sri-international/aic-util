5b9c5fdda3a788ba97dce4c5980a451ee15d537c
Bobak <Bobak@DESKTOP-E9VL9RK>
2018-08-03 23:44:07 -0700

    - added timeAndGetResult(.)

--- src/main/java/com/sri/ai/util/Timer.java
+++ src/main/java/com/sri/ai/util/Timer.java
@@ -1,6 +1,7 @@
 package com.sri.ai.util;
 
 import com.sri.ai.util.base.NullaryFunction;
+import com.sri.ai.util.base.Pair;
 
 public class Timer {
 
@@ -10,5 +11,13 @@ public class Timer {
 		long finalTime = System.currentTimeMillis();
 		long result = finalTime - initialTime;
 		return result;
+	}
+	
+	public static <T> Pair<T,Long> timeAndGetResult(NullaryFunction<T> procedure) {
+		long initialTime = System.currentTimeMillis();
+		T result = procedure.apply();
+		long finalTime = System.currentTimeMillis();
+		long time = finalTime - initialTime;
+		return new Pair<T,Long>(result,time);
 	}
 }
