2e4ddc57eed6f5e9ad2042d408ab2974cabe38d9
Bobak <Bobak@DESKTOP-E9VL9RK>
2018-08-17 10:01:10 -0700

    - expanded ExplanationRecord interface and implementations

--- src/main/java/com/sri/ai/util/explanation/logging/api/ExplanationRecord.java
+++ src/main/java/com/sri/ai/util/explanation/logging/api/ExplanationRecord.java
@@ -3,5 +3,11 @@ package com.sri.ai.util.explanation.logging.api;
 public interface ExplanationRecord {
 	
 	public Number getImportance();
+	
+	public Number getCompundedImportance();
+
+	public int getNestingDepth();
+
+	public Object[] getObjects();
 
 }
--- src/main/java/com/sri/ai/util/explanation/logging/core/DefaultExplanationRecord.java
+++ src/main/java/com/sri/ai/util/explanation/logging/core/DefaultExplanationRecord.java
@@ -4,14 +4,36 @@ import com.sri.ai.util.explanation.logging.api.ExplanationRecord;
 
 public class DefaultExplanationRecord implements ExplanationRecord {
 	
+	final double importance;				// raw marked importance (relative to block it record was extracted from)
+	final double compundedImportance;		// overall importance (importance values having been multiplied when entering each successive explanation block)
+	final int nestingDepth;					// depth of nesting record was extracted from within nested start/end explanation blocks
+	final Object[] objects;					// objects that were recorded from the explanation
+	
 	public DefaultExplanationRecord(double importance, double compoundedImportance, int nestingDepth, Object[] objects) {
-		// TODO Auto-generated constructor stub
+		this.importance = importance;
+		this.compundedImportance = compoundedImportance;
+		this.nestingDepth = nestingDepth;
+		this.objects = objects;
 	}
 
 	@Override
 	public Number getImportance() {
-		// TODO Auto-generated method stub
-		return null;
+		return importance;
+	}
+
+	@Override
+	public Number getCompundedImportance() {
+		return compundedImportance;
+	}
+
+	@Override
+	public int getNestingDepth() {
+		return nestingDepth;
+	}
+
+	@Override
+	public Object[] getObjects() {
+		return objects;
 	}
 
 }
