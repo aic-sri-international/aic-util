3aeb4deab5e6991c818d8b997c69c5f8aaae5020
ddunkley <david.dunkley@sri.com>
2018-10-13 08:14:11 -0700

    add validation

--- src/main/java/com/sri/ai/util/graph2d/core/values/DefaultSetOfValues.java
+++ src/main/java/com/sri/ai/util/graph2d/core/values/DefaultSetOfValues.java
@@ -7,13 +7,14 @@ import java.util.Iterator;
 
 import com.sri.ai.util.graph2d.api.variables.SetOfValues;
 import com.sri.ai.util.graph2d.api.variables.Value;
+import org.apache.commons.lang3.Validate;
 
 public class DefaultSetOfValues implements SetOfValues {
 	
 	private Collection<Value> collection;
 	
 	public DefaultSetOfValues(Collection<Value> collection) {
-		this.collection = collection;
+		this.collection = Validate.notEmpty(collection);
 	}
 	
 	@Override
