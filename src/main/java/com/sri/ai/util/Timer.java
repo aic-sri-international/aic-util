1dae698cf685079ae67767344a226be11fe6f83e
Rodrigo de Salvo Braz <braz@ai.sri.com>
2020-07-08 11:04:39 -0300

    - a new convenience method in Timer.

--- src/main/java/com/sri/ai/util/Timer.java
+++ src/main/java/com/sri/ai/util/Timer.java
@@ -1,5 +1,6 @@
 package com.sri.ai.util;
 
+import static com.sri.ai.util.Util.println;
 import static com.sri.ai.util.Util.round;
 
 import com.sri.ai.util.base.NullaryFunction;
@@ -14,7 +15,7 @@ public class Timer {
 		long result = finalTime - initialTime;
 		return result;
 	}
-	
+
 	public static <T> Pair<T,Long> getResultAndTime(NullaryFunction<T> procedure) {
 		long initialTime = System.currentTimeMillis();
 		T result = procedure.apply();
@@ -22,7 +23,17 @@ public class Timer {
 		long time = finalTime - initialTime;
 		return new Pair<T,Long>(result,time);
 	}
-	
+
+	public static <T> T printTime(String description, NullaryFunction<T> procedure) {
+		return printTime(description, procedure, 3);
+	}
+
+	public static <T> T printTime(String description, NullaryFunction<T> procedure, int decimalPlaces) {
+		var resultAndTime = getResultAndTime(procedure);
+		println(description + timeStringInSeconds(resultAndTime, decimalPlaces) + " seconds");
+		return resultAndTime.first;
+	}
+
 	public static <T> String timeStringInSeconds(Pair<T,Long> resultAndTime, int decimalPlaces) {
 		return round(resultAndTime.second/1000., decimalPlaces) + " seconds";
 	}
